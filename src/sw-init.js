5f959deadfdac362a94afee559ba29b712961820
Tony Trinh <tony19@gmail.com>
2021-07-11 20:31:34 -0500

    show notification from service worker

--- src/sw-init.js
+++ src/sw-init.js
@@ -1,8 +1,18 @@
 import registerServiceWorker, { ServiceWorkerNoSupportError } from 'service-worker-loader!./sw'
 
+function showNotification(registration, { title, body }) {
+  Notification.requestPermission(result => {
+    if (result === 'granted') {
+      registration.showNotification(title, {
+        body,
+        requireInteraction: true,
+      })
+    }
+  })
+}
+
 registerServiceWorker({ scope: '/' }).then(registration => {
-  console.log('Success!')
-  console.log(registration)
+  showNotification(registration, { title: 'Greetings', body: 'Hello from your Service Worker' })
 }).catch(err => {
   if (err instanceof ServiceWorkerNoSupportError) {
     console.log('Service worker is not supported.')
